27f90094d7edb6970adbf488d4fb5173848f44f6
Chinedu Eze <michaelanayo64@gmail.com>
2025-05-24 19:44:45 +0100

    updated code

--- firstpython.py
+++ firstpython.py
@@ -1,3 +1,10 @@
 #Display the output
 
-print('New python file')
+def print_moji(ref):
+    if ref > 0 and ref < 6:
+        print('Nedu happy😁')
+    else:
+        print('Nedu sad😭')
+        
+print_moji(1)
+print_moji(6)
